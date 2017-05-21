34fcb3116f094531f918dcef9cfa55e765d836c2
Ed Cashin <ed.cashin@acm.org>
2017-05-21 08:39:47 -0400

    Clean up more kinds of records without over generalizing

--- privacyrights/prep-rb.R
+++ privacyrights/prep-rb.R
@@ -13,9 +13,13 @@ prep.rb <- function(fnam="select-all.csv") {
     x <- d$Records.Breached
 
     do.not.know <- c(
+        "^among the",
         "^unknown",
+        "^unkown",                      # I guess I shouldn't be surprised.
+        "^unknowm",
         "^hundreds",
         "^\"hundreds\"",
+        "^tens\\s+",
         "^thousands",
         "^at least",
         "^more than",
@@ -23,8 +27,11 @@ prep.rb <- function(fnam="select-all.csv") {
         "^considerably more than",
         "^up to ",
         "^fewer than ",
+        "^nearly ",
         "^between ",
-        "^not disclosed"
+        "^not disclosed",
+        "^Very few of",
+        "^(\\d+)\\s*-\\s*(\\d+)"
     )        
     for (re in do.not.know) {
         omit[grep(re, x, ignore.case=T)] <- TRUE
@@ -43,19 +50,33 @@ prep.rb <- function(fnam="select-all.csv") {
     x <- gsub("\\(not .*ssn.*\\)", "", x, ignore.case=T)
     x <- gsub("\\(no .*ssn.*\\)", "", x, ignore.case=T)
     x <- gsub("\\(not .*total.*\\)", "", x, ignore.case=T)
+    x <- gsub("[ -]*not .*total.*", "", x, ignore.case=T)
+    x <- gsub("[ -]*not .*number.*", "", x, ignore.case=T)
     x <- gsub("\\(no .*total.*\\)", "", x, ignore.case=T)
     x <- gsub("\\(no incidents reported\\)", "", x, ignore.case=T)
     x <- gsub("\\(not added to total\\)", "", x, ignore.case=T)
     x <- gsub("not added to total", "", x, ignore.case=T)
     x <- gsub("[0-9]+,*\\supdated amount ([0-9,]+)", "\\1", x, ignore.case=T, perl=T)
-    x <- gsub("(\\d+)\\s+not.*total", "\\1", perl=T, ignore.case=T, x)
+    x <- gsub("([0-9]+)\\(updated as of.*", "\\1", x, ignore.case=T, perl=T)
+    x <- gsub("(\\d+)\\s+not.*total.*", "\\1", perl=T, ignore.case=T, x)
+    x <- gsub("(\\d+)\\s+\\(not.*total.*", "\\1", perl=T, ignore.case=T, x)
+    x <- gsub("(\\d+)\\s+\\(not.*included.*", "\\1", perl=T, ignore.case=T, x)
+    x <- gsub("(\\d+)\\s+not.*included.*", "\\1", perl=T, ignore.case=T, x)
+    x <- gsub("^\\s*(\\d+) \\(.*credit card.*", "\\1", x, perl=T, ignore.case=T)
     x <- str_trim(x)
     x <- gsub("^(\\d+)\\s+because it's included.*", "\\1", x, perl=T, ignore.case=T)
     x <- gsub("^(\\d+)\\s+.*SSN.*", "\\1", x, perl=T)
+    x <- gsub("^(\\d+)\\s+\\(at least \\1\\s+.*", "\\1", x, perl=T, ignore.case=T)
+    x <- gsub("^1800 [+] 23$", "1823", x)
+    x <- gsub("\\s*15806, 25000 more later discovered\\s*", "40806", x)
 
     # "5247. It does not appear that SSNs or financial account                numbers were exposed."
     # "280000. It is not clear that SSNs or financial account                numbers were exposed." 
     x <- gsub("^([0-9]+)\\..*", "\\1", x)
+
+    x <- gsub(".* raised to ", "", x, ignore.case=T)
+    x <- gsub("^\\s*(\\d+)\\s+million", "\\1000000", x, ignore.case=T, perl=T)
+    x <- gsub("^\\s+12\\.5 million", "12500000", x, ignore.case=T, perl=T)
 
     for (i in c(
         "households", "students", "patients", "current", "children",
@@ -64,16 +85,27 @@ prep.rb <- function(fnam="select-all.csv") {
         "north carolina", "former", "victims", "magazine",
         "RNC", "records", "upward bound students", "fund shareholders",
         "assembly members", "policyholders", "individuals",
-        "in Hawaii", "Con Edison"
+        "in Hawaii", "Con Edison", "[[]Updated", "clients",
+        "Oregon high school", "New Yorkers", "\\(\\d+ cases of",
+        "\\(\\d+ employees", "\\(\\d+ confirmed", "business emails",
+        "\\(unknown number", "residents", "\\(between", "\\(two social",
+        "\\(\\d+ people", "\\(\\d+ checks", "as communicated",
+        "NYSEG customers", "\\(\\d+ encrypted", "american accounts",
+        "\\(\\d+ individuals", "\\(\\d*\\s*partial", "\\(nine social",
+        "\\(three social", "\\(107\\)", "\\.\\s+updated as of",
+        "\\(no social security", "\\(last four", "\\(\\d+ customers"
     )) {
         re <- paste("^([0-9]+)\\s", i, ".*", sep="")
-        x <- gsub(re, "\\1", x, ignore.case=T)
+        x <- gsub(re, "\\1", x, ignore.case=T, perl=T)
     }
 
-    x <- gsub(".* raised to ", "", x, ignore.case=T)
+    x <- gsub("^(\\d+); \\d+ social security.*", "\\1", x, ignore.case=T, perl=T)
+    x <- gsub("^(\\d+) \\(\\d+ social security.*", "\\1", x, ignore.case=T, perl=T)
     x <- gsub("^([0-9]+)\\s*\\n.*", "\\1", x)
-
-    x <- gsub("^\\s*(\\d+)\\s+million", "\\1000000", x, ignore.case=T, perl=T)
+    x <- gsub("[[](\\d+)[]]", "\\1", x)
+    x <- gsub("^(\\d+), although.*", "\\1", x, ignore.case=T, perl=T)
+    x <- gsub("^(\\d+) \\(number includes.*", "\\1", x, ignore.case=T, perl=T)
+    x <- gsub("^individuals in (\\d+) households.*", "\\1", x, ignore.case=T, perl=T)
 
     omit[grep("^\\d+\\s*-\\s*\\d+$", x, perl=T)] <- TRUE
     omit[x == ""] <- TRUE
